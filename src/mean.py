5256e6c60eff89a87e027d913829d0564bba3728
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 18:40:36 +0200

    Task 10 done.

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,19 @@
 def mean(number):
   # Type your code
-  return 
+    number_str = str(number)
+
+    sum_of_digits = 0
+    count_of_digits = 0
+
+    for digit in number_str:
+
+        if digit.isdigit():
+            sum_of_digits += int(digit)
+            count_of_digits += 1
+
+
+    if count_of_digits > 0:
+        mean_value = sum_of_digits / count_of_digits
+        return mean_value
+    else:
+        return 0
