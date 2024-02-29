56c352f56cf261562f3028ca8cfa17ac93f09499
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 17:47:31 +0200

    Task 3 done.

--- src/squares_sum.py
+++ src/squares_sum.py
@@ -1,5 +1,5 @@
-def squares_sum(base, height):
-  # Type your code
-  area = (base * height) / 2
-  return area
+def tri_area(base, height):
+# Type your code
+    area = (base * height) / 2
+    return area
 
