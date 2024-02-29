9fb71017a697351247c7800c82e8d1624c4046a6
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 17:51:47 +0200

    Task 4 done.

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,3 +1,7 @@
 def calculate_fuel(distance):
-  # Type your code
-  return
+    # Розрахунок обсягу палива
+    fuel = distance * 10
+    # Перевірка, щоб обсяг палива був не менше 100
+    if fuel < 100:
+        fuel = 100
+    return fuel if distance > 0 else 0
