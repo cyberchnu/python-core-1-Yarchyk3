49866878996b326f68dee5ca125019b3896a84cd
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 18:35:38 +0200

    Task 9 done.

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,10 @@
 def sum_even_nums_in_range(start, stop):
   # Type your code
-  return
+  sum_even = 0
+
+
+  for num in range(start, stop):
+    if num % 2 == 0:
+      sum_even += num
+
+  return sum_even
