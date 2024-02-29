b39443ef6b107b6d2a80b9aa913ae9614fbbfa69
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 17:59:13 +0200

    Task 5 done.

--- src/both.py
+++ src/both.py
@@ -1,3 +1,7 @@
 def both(number1, number2):
-  # Type your code
-  return
+    if (number1 <= 0 and number2 <= 0) or (number1 >= 0 and number2 >= 0):
+        return True
+    elif number1 == 0 and number2 == 0:
+        return True
+    else:
+        return False
