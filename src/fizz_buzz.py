e06baa054eb9f55e07c39c9934db972cc0186310
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 17:27:25 +0200

    Task 2 done.

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,11 @@
-def fizz_buzz(param):
+def fizz_buzz(number):
   #Type your code here
-  return
+  if number % 3 == 0 and number % 5 == 0:
+    return "FizzBuzz"
+  elif number % 3 == 0:
+    return "Fizz"
+  elif number % 5 == 0:
+    return "Buzz"
+  else:
+    return number
+
