553a4339684fb1dfc8168041008c4709bb946da4
Yarchyk3 <159487862+Yarchyk3@users.noreply.github.com>
2024-02-29 18:09:21 +0200

    Task 6 done.

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,5 @@
 def int_within_bounds(number, lower_bound, upper_bound):
-  # Type your code
-  return 
+    if isinstance(number, int):
+        return lower_bound <= number < upper_bound
+    else:
+        return False
